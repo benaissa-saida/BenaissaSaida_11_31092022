d3e49f9fb563133d89a61e4efefc5bf97b4b6e14
benaissa-saida <benaissasaida28@gmail.com>
2022-11-14 19:45:55 +0100

    cards add in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import Layout from '../components/Layout';
 import Banner from '../components/Banner';
 import coverHome from '../assets/coverHome.png';
+import Cards from '../components/Cards';
+import logements from '../datas/logements';
 
 function Home() {
    return (
@@ -9,6 +11,7 @@ function Home() {
             <img className="ks-cover-img" src={coverHome} alt="background" />
             <h1 className="ks-motto">Chez vous, partout et ailleurs</h1>
          </Banner>
+         <Cards data={logements} />
       </Layout>
    );
 }
