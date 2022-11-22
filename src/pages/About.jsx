3bba344b535f1fbbcac63b53b88a23f96a6ed61b
benaissa-saida <benaissasaida28@gmail.com>
2022-11-22 16:29:04 +0100

    about page done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,10 @@
 import Layout from '../components/Layout';
 import Banner from '../components/Banner';
 import coverAbout from '../assets/coverAbout.png';
+import abouts from '../datas/about';
 import { useEffect } from 'react';
+import Dropdown from '../components/Dropdown';
+import '../styles/About.css';
 
 function About() {
    useEffect(() => {
@@ -9,9 +12,21 @@ function About() {
    }, []);
    return (
       <Layout>
-         <Banner className="banner-container">
+         <Banner>
             <img className="ks-cover-img" src={coverAbout} alt="background" />
          </Banner>
+         <main>
+            <div className="ks-dropdown-container">
+               {abouts.map((about) => (
+                  <Dropdown
+                     key={about.title}
+                     title={about.title}
+                     content={about.content}
+                     nameOfClass="ks-dropdown-about"
+                  />
+               ))}
+            </div>
+         </main>
       </Layout>
    );
 }
